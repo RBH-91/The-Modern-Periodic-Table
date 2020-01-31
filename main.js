f22cce2dc6ca6da64a307540523993f69f742361
Rahul <51887323+RBH-91@users.noreply.github.com>
2020-01-31 21:51:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,40 @@
 let output;
-function findByNum(num,check) {
-    if(check==false){
-        
-        if (!elements[num - 1]) 
-        { output='Check for errors, no such element exists.';
-         document.getElementById('output').innerHTML=output;
-         return 'Over!';
+function findByNum(num, check) {
+    if (check == false) {
+        if (!elements[num - 1]) {
+            output = 'Check for errors, no such element exists.'; document.getElementById('output').innerHTML = output;
+            return 'Over!';
         }
-        output=`The element is ${elements[num - 1].name} \n`
-                                +
-                `Symbol: ${elements[num - 1].sym} \n`
-                                +
-        `Group: ${elements[num - 1].group} Period: ${elements[num - 1].period} \n`;
-        
-        document.getElementById('output').innerHTML=output;
-        
+        output = `The element is ${elements[num - 1].name} \n`
+            +
+            `Symbol: ${elements[num - 1].sym} \n`
+            +
+            `Group: ${elements[num - 1].group} Period: ${elements[num - 1].period} \n`;
+        document.getElementById('output').innerHTML = output;
         return 'Enjoy!';
     }
 }
-
-
-function findByName(name) {
+function find(name) {
     let yes;
     for (let i = 0; i < elements.length; i++) {
         if (elements[i].sym == name) {
-            output=`The element is ${elements[i].name} \n`
-                                    +
-                        `Symbol: ${elements[i].sym} \n`
-                                    +
-            `Group: ${elements[i].group}   Period: ${elements[i].period} \n`;
-            document.getElementById('output').innerHTML=output
+            output = `The element is ${elements[i].name} \n`
+                +
+                `Symbol: ${elements[i].sym} \n`
+                +
+                `Group: ${elements[i].group}   Period: ${elements[i].period} \n`;
+            document.getElementById('output').innerHTML = output
             yes = name;
             break;
         }
 
     }
     if (yes) { return 'Enjoy' }
-    else {return false; };
+    else { return false; };
 }
-
-//for HTML part.
-
-function execute(input){
-    findByName(`${input}`);
-    findByNum(input,findByName(`${input}`));
+function execute(input) {
+    find(`${input}`);
+    findByNum(input, find(`${input}`));
 }
 
 
