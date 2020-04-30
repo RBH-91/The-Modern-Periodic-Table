5e82d43b455b3c3c29f2bf5c7c05e898604e7c44
Rahul <51887323+RH-91@users.noreply.github.com>
2020-04-30 10:15:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ function findByNum(num, check) {
             output = 'Check for errors, no such element exists.'; document.getElementById('output').innerHTML = output;
             return 'Over!';
         }
-        output = `Atomic No: ${num}\n
-             The element is ${elements[num - 1].name} \n`
+        output = `The element is ${elements[num - 1].name} \n`
             +
             `Symbol: ${elements[num - 1].sym} \n`
+            +
+            `Atomic No:${num}`
             +
             `Group: ${elements[num - 1].group} Period: ${elements[num - 1].period} \n`;
         document.getElementById('output').innerHTML = output;
